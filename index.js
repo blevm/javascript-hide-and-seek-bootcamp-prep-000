5c925e70dee8a52cc4bfbeb1b0205221040eee3e
Michelle B <mjblevins7@gmail.com>
2017-12-22 03:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ function deepestChild() {
   function find(array, criteriaFn) {
   let current = array
   let next = []
- 
+
   while (current) {
 
     if (criteriaFn(current)) {
       return current
     }
- 
+
     if (Array.isArray(current)) {
       for (let i = 0; i < current.length; i++) {
         next.push(current[i])
       }
     }
- 
+
     current = next.shift()
   }
   return current;
