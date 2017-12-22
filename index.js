c9f5e7937b7986caca1c76444613fe387a6e3783
Michelle B <mjblevins7@gmail.com>
2017-12-22 03:30:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('.ranked-list');
 
   for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + n).toString()
+  lis[i].innerHTML = (i + n).toString();
   }
 }
 
 function deepestChild() {
-
-  let grandNode = document.getElementById('grand-node')
-  let nextNode = grandNode.children[0]
+  let grandNode = document.getElementById('grand-node');
+  let nextNode = grandNode.children[0];
 
   while (nextNode) {
-    grandNode = nextNode
-    nextNode = grandNode.children[0]
+    grandNode = nextNode;
+    nextNode = grandNode.children[0];
 }
-return grandNode;
+  return grandNode;
 }
