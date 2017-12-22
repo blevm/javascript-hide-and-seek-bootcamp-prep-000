1c036ce8c7afb8b3483f9d3e5d10641bb67ebec4
Michelle B <mjblevins7@gmail.com>
2017-12-22 03:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function increaseRankBy(n) {
   lis[i].innerHTML = (i + n).toString()
   }
 }
+
+function deepestChild() {
+  const grand = document.getElementById('grand-node').querySelectorAll()
+
+  function find(array, criteriaFn) {
+  let current = array
+  let next = []
+ 
+  while (current) {
+
+    if (criteriaFn(current)) {
+      return current
+    }
+ 
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
+ 
+    current = next.shift()
+  }
+  return current;
+}
