3a4cda33d517eed8e4a1eda6f778e8dad0d4c6c7
Michelle B <mjblevins7@gmail.com>
2017-12-22 03:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target')
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementByClassName('.ranked-list')
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString()
+}
+}
